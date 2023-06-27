f6defe94aee0daaa7a0db04d74b8c8503a83cea9
lucaoskaique <lucas.kaique175@hotmail.com>
2023-06-27 11:17:30 -0300

    Fixed open graps meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,22 +5,25 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="https://res.cloudinary.com/lucaos/image/upload/c_thumb,w_200,g_face/v1687875120/COICE_PERFIL_ugvaio.png" />
           <meta
             name="description"
-            content="See pictures from COICE."
+            content="Exploring percussive club music."
           />
           <meta property="og:site_name" content="coice.com.br" />
           <meta
             property="og:description"
-            content="See pictures from COICE."
+            content="Exploring percussive club music."
           />
+          <meta property="og:url" content="http://coice.com.br" />
           <meta property="og:title" content="COICE 2023 Pictures" />
+          <meta property="og:image" content="https://res.cloudinary.com/lucaos/image/upload/v1687867589/capacoice.png" />
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:title" content="COICE 2023 Pictures" />
+          <meta name="twitter:creator" content="@coic_e" />
           <meta
             name="twitter:description"
-            content="See pictures from COICE."
+            content="Exploring percussive club music."
           />
         </Head>
         <body className="bg-black antialiased">
