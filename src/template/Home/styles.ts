8dc6d25a1c9a08a3e53bea607c4458bea088ec19
lucaoskaique <lucas.kaique175@hotmail.com>
2023-12-20 01:38:20 -0300

    Deleted coments

--- src/template/Home/styles.ts
+++ src/template/Home/styles.ts
@@ -1,29 +1,25 @@
 import styled from 'styled-components';
 
-// Estilo para o container principal
 export const MainContainer = styled.main`
   margin: 0 auto;
-  max-width: 1960px; // ou outro valor conforme sua necessidade
+  max-width: 1960px; 
   padding: 1rem;
 `;
 
 // Estilo para a grade de imagens
 export const ImageGrid = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr)); // Ajuste o tamanho mínimo e máximo da coluna como necessário
-  gap: 1rem; // Espaçamento entre as imagens
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr)); 
+  gap: 1rem; 
 
   @media (max-width: 640px) {
     grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
   }
-
-  // Estilize mais se necessário, por exemplo, para hover effects, etc.
 `;
 
 // Estilo para o footer, se necessário
 export const StyledFooter = styled.footer`
   padding: 1rem;
   text-align: center;
-  background-color: #f3f3f3; // Exemplo de cor de fundo
-  // Outros estilos para o footer
+  background-color: #f3f3f3; 
 `;
