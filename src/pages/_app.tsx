298c8cf00d80aed1243b235967052f8a468ba141
lucaoskaique <lucas.kaique175@hotmail.com>
2024-09-21 23:41:28 -0300

    refactor: Update page title and meta tags
    Update the page title and meta tags in the _app.tsx file to reflect the new branding and provide a more accurate description of COICE PROD's services. Also, include relevant keywords for search engine optimization. This commit improves the SEO and branding of the application.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,30 +8,50 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Head>
-        <title>COICE Photos</title>
+        <title>COICE PROD | Technology Solutions for Events</title>
         <link
           rel="icon"
           href="https://res.cloudinary.com/lucaos/image/upload/c_thumb,w_200,g_face/v1687875120/COICE_PERFIL_ugvaio.png"
         />
-        <meta name="description" content="Exploring percussive club music." />
-        <meta property="og:site_name" content="coice.com.br" />
+        <meta
+          name="description"
+          content="COICE PROD specializes in event production and innovative technology solutions for unforgettable experiences. Based in Porto Alegre, we blend music, programming, and visual arts."
+        />
+        <meta
+          name="keywords"
+          content="event production, event technology, Porto Alegre events, music production, visual arts, performance technology"
+        />
+        <meta property="og:site_name" content="COICE PROD" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://coice.com.br" />
+        <meta
+          property="og:title"
+          content="COICE PROD | Innovative Event Production & Technology"
+        />
         <meta
           property="og:description"
-          content="Exploring percussive club music."
+          content="Discover COICE PROD's cutting-edge event production and technology solutions. We create immersive experiences blending music, programming, and visual arts in Porto Alegre."
         />
-        <meta property="og:url" content="http://coice.com.br" />
-        <meta property="og:title" content="COICE 2023 Pictures" />
         <meta
           property="og:image"
           content="https://res.cloudinary.com/lucaos/image/upload/v1687867589/capacoice.png"
         />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="COICE 2023 Pictures" />
+        <meta name="twitter:site" content="@coic_e" />
         <meta name="twitter:creator" content="@coic_e" />
         <meta
-          name="twitter:description"
-          content="Exploring percussive club music."
+          name="twitter:title"
+          content="COICE PROD | Innovative Event Production & Technology"
         />
+        <meta
+          name="twitter:description"
+          content="COICE PROD delivers cutting-edge event production and technology solutions in Porto Alegre, creating immersive experiences that blend music, programming, and visual arts."
+        />
+        <meta
+          name="twitter:image"
+          content="https://res.cloudinary.com/lucaos/image/upload/v1687867589/capacoice.png"
+        />
+        <link rel="canonical" href="https://coice.com.br" />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
